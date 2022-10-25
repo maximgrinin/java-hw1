2c8a67b497cc8590d7bbff247781bf3e1900739c
maximgrinin <maximgrinin@gmail.com>
2022-10-25 13:15:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Я умею выводить строчки");
+        System.out.println("Я начал программировать");
+        System.out.printf("67 / 4 = %.2f\n", (float) 67 / 4);
+        System.out.printf("450  * 2 = %d\n", 450  * 2);
+        System.out.printf("1234 * 1234 = %d\n", 1234 * 1234);
+    }
+}
