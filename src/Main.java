7b393446de9195abfd73d01cf31f0d7f2f5e940c
maximgrinin <maximgrinin@gmail.com>
2022-10-26 20:47:35 +0300

    Fix

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Я умею выводить строчки");
         System.out.println("Я начал программировать");
         System.out.printf("67 / 4 = %.2f\n", (float) 67 / 4);
-        System.out.printf("450  * 2 = %d\n", 450  * 2);
+        System.out.printf("450 * 2 = %d\n", 450 * 2);
         System.out.printf("1234 * 1234 = %d\n", 1234 * 1234);
     }
 }
